c6cbf9f9487ac21283d03787f0ef3fe564b5a1c3
aelimon <40379087+aelimon@users.noreply.github.com>
2018-06-19 01:10:28 -0400

    Add files via upload

---
+++ P2T1_ SalesPrediction_LimonAlisabeth.py.py
@@ -0,0 +1,8 @@
+Python 3.6.5 (v3.6.5:f59c0932b4, Mar 28 2018, 16:07:46) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> total_sales=float(input('Enter the projected sales: '))
+Enter the projected sales: 10000
+>>> profit=total_sales * 0.23
+>>> print('The profit is $', format(profit, ',.2f'))
+The profit is $ 2,300.00
+>>> 
